fdeac86580869522c5338533f24fbc48699f878d
20203174 <kaeun0000@naver.com>
2023-10-13 20:30:15 +0900

    singleton pattern study

---
+++ designPattern/src/main/java/org/example/singleton/gaeun/bank/IAccount.java
@@ -0,0 +1,8 @@
+package org.example.singleton.gaeun.bank;
+
+import java.math.BigDecimal;
+
+public interface IAccount {
+    BigDecimal deposit(BigDecimal amount);
+    boolean withDraw(BigDecimal amount);
+}
